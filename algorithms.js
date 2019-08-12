300b53f1c8ae4581e6475e0178e0203ebff8c8e5
bavarianrhino <rjriesenberger@gmail.com>
2019-08-12 13:25:13 -0400

    completed factorialize

--- algorithms.js
+++ algorithms.js
@@ -70,12 +70,20 @@ function reverseString(str) {
 //###################################################################################
 //Factorialize a Number
 function factorialize(num) {
-	
+    if (num === 0) {
+        return 1
+    } else {
+        let multipliedNum = 1
+        for(let i = num; i > 1; i--){
+            multipliedNum = multipliedNum * i;
+        }
+        return multipliedNum
+    }
 }
-// factorialize(0); // should return 1.
-// factorialize(5); // should return 120.
-// factorialize(10); // should return 3628800.
-// factorialize(20); // should return 2432902008176640000.
+// console.log(factorialize(0)); // should return 1.
+// console.log(factorialize(5)); // should return 120.
+// console.log(factorialize(10)); // should return 3628800.
+// console.log(factorialize(20)); // should return 2432902008176640000.
 
 //###################################################################################
 // Check for Palindrome
